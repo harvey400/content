661b25f2254709cf3c51890026262487e791bbee
Harvey <harveytapang@gmail.com>
2024-06-21 17:11:03 +0800

    Modify temporarily HomeView

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('../components/contents/2/Index.vue')
     },
     {
       path:'/1',
